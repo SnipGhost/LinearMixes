d89801bdd91c888e03c301ff6b9b4b5546abeee7
SnipGhost <snipghost@list.ru>
2016-12-03 21:25:46 +0400

    Remaked and fixed FAIL bug

--- LinearMixes/main.cpp
+++ LinearMixes/main.cpp
@@ -1,78 +1,54 @@
-#define WINVER 0x0501
-#define _WIN32_WINNT 0x0501
-#define _WIN32_WINDOWS 0x0501
-#define _WIN32_IE 0x0600
 #include <iostream>
 #include <fstream>
+#define GETBIT(x,pos) ( ((x) & ( 0x1 << (pos) )) !=0 )
 using namespace std;
+typedef double TYPE;
 int main()
 {
-	double EPS = 0.00000000000001;
 	system("chcp 1251 > nul");
-	double *keys, num;
-	int count, SIZE = 1;
-	bool *solve, ret = false;
+	double EPS = 0.00000000000000001;
+	TYPE *keys, num, sum;
+	unsigned SIZE = 1, count, i;
+	unsigned long int solve;
+	bool end = false;
 	cout << "Введите число: ";
 	cin >> num;
 	cout << "Введите количество чисел в массиве: ";
 	cin >> SIZE;
-	keys = new double[SIZE];
-	solve = new bool[SIZE];
+	if (SIZE > 32) {
+		cout << "Слишком много эл-ов! (Не более 32)\n";
+		return 0;
+	}
+	keys = new TYPE[SIZE];
 	cout << "Введите массив: ";
-	for (int i = 0; i < SIZE; ++i)
-		cin >> keys[i];
-	for (int i = 0; i < SIZE; ++i)
+	for (i = 0; i < SIZE; ++i) cin >> keys[i];
+	//---
+	for (solve = 0; solve <= pow(2, SIZE); ++solve)
 	{
-		int m = i;
-		for (int j = i+1; j < SIZE; ++j)
-		{
-			if (keys[j] > keys[m]) 
-			{
-				m = j;
-			}
-		}
-		double buf = keys[i];
-		keys[i] = keys[m];
-		keys[m] = buf;
-		cout << keys[i] << " ";
-	}
-	cout << endl;
-	for (int i = 0; (i < SIZE)&&(!ret); ++i)
-	{
-		for (int k = 0; k < SIZE; ++k) solve[k] = false;
-		if (keys[i] > num) continue;
-		double sum = 0;
 		count = 0;
-		for (int j = i; j < SIZE; ++j)
+		sum = 0;
+		for (i = 0; i < SIZE; ++i)
 		{
-			sum += keys[j];
-			solve[j] = true;
-			count++;
-			if (abs(sum - num) < EPS) {
-				ret = true; 
-				break;
+			if (GETBIT(solve, i) == 1)
+			{
+				sum += keys[i];
+				count++;
 			}
-			else if (sum > num) {
-				sum -= keys[j];
-				solve[j] = false;
-				count--;
+		}
+		if (abs(sum - num) < EPS) {
+			for (i = 0; i < SIZE; ++i) 
+			{
+				if (GETBIT(solve, i) == 1) {
+					cout << keys[i];
+					count--;
+					if (count > 0) cout << " + ";
+					else cout << " = " << num << endl;
+				}
 			}
 		}
 	}
-	if (!ret) {
-		cout << "Нет решений" << endl;
-	} else {
-		cout << "Решение для " << count << " чисел" << endl;
-		for (int k = 0; k < SIZE; ++k)
-		{
-			if (solve[k]) {
-				cout << keys[k];
-				count--;
-				if (count > 0) cout << " + ";
-			}
-		}
-		cout << " = " << num << endl;
-	}
+	//---
+	cout << "Больше нет решений\n";
 	system("pause");
 	delete [] keys;
 	return 0;
